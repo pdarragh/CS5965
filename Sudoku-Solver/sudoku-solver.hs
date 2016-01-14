ccf45230229878121efba3b9534e9161559e3ed0
Pierce Darragh <pierce.darragh@gmail.com>
2016-01-14 10:52:22 -0700

    Successful File I/O
    Can now read and write properly. Hooray! Some flow changes ensued. Sigh.

--- Sudoku-Solver/sudoku-solver.hs
+++ Sudoku-Solver/sudoku-solver.hs
@@ -10,12 +10,16 @@ TODO:
     * 'generate' - create a new board
 -}
 
+import Control.Monad
 import Data.List
 import Data.List.Split
 import System.Directory
 import System.Environment
 import System.Exit
 import System.IO
+
+type Board = [[Int]]
+type Row = [Int]
 
 -- print_error
 --
@@ -25,14 +29,33 @@ print_error message = do
     putStrLn ("Error: " ++ message)
     exitFailure
 
+-- print_row
+--
+-- Takes a row and prints it out nicely. Replaces 0 with '_'.
+print_row :: Row -> String
+print_row row = intercalate " " $ map show row
+
+-- print_board
+--
+-- Takes a board and prints it out nicely. Replaces 0 with '_'.
+print_board :: Board -> IO ()
+print_board board = putStrLn $ intercalate "\n" $ map print_row board
+
+main :: IO ()
 main = do
     args <- getArgs
+    let filename = args !! 0
     if (length args) /= 1 then do
         print_error "Accepts one argument: the name of the file to read."
     else do
         -- Now do the file reading.
-        let filename = args !! 0
-        read_file filename
+        --let board = read_file filename
+        file_exists <- doesFileExist filename
+        if file_exists then do
+            -- It does exist! Let's load it.
+            contents <- readFile filename
+            print_board $ build_board_from_lines $ filter (not . null) $ splitOn "\n" contents
+        else error "Invalid filename."
 
 -- replace_underscore_char
 --
@@ -47,6 +70,19 @@ replace_underscore_char c = c
 replace_underscore :: String -> String
 replace_underscore string = map replace_underscore_char string
 
+-- replace_zero_char
+--
+-- Returns a character given, unless that character is a zero.
+replace_zero_char :: Char -> Char
+replace_zero_char '0' = '_'
+replace_zero_char c = c
+
+-- replace_zero
+--
+-- Replaces all instances of '0' within a string with '_' for output formatting.
+replace_zero :: String -> String
+replace_zero string = map replace_zero_char string
+
 -- read_numbers_from_line
 --
 -- Given a space-separated line of Sudoku numbers, returns a list of integers.
@@ -54,48 +90,38 @@ replace_underscore string = map replace_underscore_char string
 -- Example:
 --   read_sudoku_line "1 2 3 4 5 6 7 8 9"
 --     -> [1, 2, 3, 4, 5, 6, 7, 8, 9]
-read_numbers_from_line :: String -> [Int]
+read_numbers_from_line :: String -> Row
 read_numbers_from_line line = map (read :: String -> Int) (words $ replace_underscore line)
 
 -- build_board_from_lines
 --
 -- Given a list of strings, produces a list of lists of integers.
-build_board_from_lines :: [String] -> [[Int]]
+build_board_from_lines :: [String] -> Board
 build_board_from_lines lines = map read_numbers_from_line lines
 
 -- is_valid_sudoku_line
 --
 -- Ensures that a given line has the appropriate number of values and that they
 -- all sum to the appropriate value (45).
-is_valid_sudoku_line :: [Int] -> Bool
+is_valid_sudoku_line :: Row -> Bool
 is_valid_sudoku_line numbers =
     length numbers == (length (nub numbers)) &&
     length numbers == 9 &&
     all (>= 0) numbers &&
     sum numbers == 45
 
+extract :: (Int, Int, Int) -> [Int] -> [Int]
+extract (a, b, c) l = [(l !! a), (l !! b), (l !! c)]
+
+get_square_from_lines s lines = do
+    let values = ( s, s+1, s+2 )
+    map (extract values) lines
+
 -- is_valid_sudoku_board
 --
 -- Verifies that a board is valid.
-is_valid_sudoku_board :: [[Int]] -> Bool
-is_valid_sudoku_board board = all is_valid_sudoku_line board
-
--- read_file
---
--- Given a filename (as a string), attempts to read a sudoku board from the
--- supplied file. Verifies that the file exists.
-read_file :: String -> IO ()
-read_file filename = do
-    -- Verify the file exists.
-    file_exists <- doesFileExist filename
-    if not file_exists then do
-        -- It does not. Quit.
-        print_error ("File does not exist: '" ++ filename ++ "'")
-    else do
-        -- It does exist! Let's load it.
-        contents <- readFile filename
-        let board = build_board_from_lines $ filter (not . null) $ splitOn "\n" contents
-        if is_valid_sudoku_board board then do
-            putStrLn "Valid"
-        else do
-            putStrLn "Invalid"
+is_valid_sudoku_board :: Board -> Bool
+is_valid_sudoku_board board = do
+    let loosely_valid = all is_valid_sudoku_line board
+    let triples = [ (x, x+1, x+2) | x <- [0, 3, 6] ]
+    True
