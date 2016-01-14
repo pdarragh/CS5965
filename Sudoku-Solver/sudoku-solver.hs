9eb0807c458df2d9a6c35b2a7ed486d5f8ffd871
Pierce Darragh <pierce.darragh@gmail.com>
2016-01-13 23:35:19 -0700

    File I/O and mild verification
    Currently the program takes an input file, read a sudoku board, and "verifies" (loosely) that the board is valid.

--- Sudoku-Solver/sudoku-solver.hs
+++ Sudoku-Solver/sudoku-solver.hs
@@ -1,7 +1,17 @@
 -- 
 -- Sudoku Solver
 --
+{-
+TODO:
+  * finish implementing is_valid_sudoku_board
+  * add command line arguments
+    * 'verify' - determines whether a completed board is valid
+    * 'solve' - solve a given board
+    * 'generate' - create a new board
+-}
 
+import Data.List
+import Data.List.Split
 import System.Directory
 import System.Environment
 import System.Exit
@@ -24,6 +34,52 @@ main = do
         let filename = args !! 0
         read_file filename
 
+-- replace_underscore_char
+--
+-- Returns a character given, unless that character is an underscore.
+replace_underscore_char :: Char -> Char
+replace_underscore_char '_' = '0'
+replace_underscore_char c = c
+
+-- replace_underscore
+--
+-- Replaces all instances of '_' within a string with '0' for sudoku formatting.
+replace_underscore :: String -> String
+replace_underscore string = map replace_underscore_char string
+
+-- read_numbers_from_line
+--
+-- Given a space-separated line of Sudoku numbers, returns a list of integers.
+-- '_' will be converted to 0 in the list.
+-- Example:
+--   read_sudoku_line "1 2 3 4 5 6 7 8 9"
+--     -> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+read_numbers_from_line :: String -> [Int]
+read_numbers_from_line line = map (read :: String -> Int) (words $ replace_underscore line)
+
+-- build_board_from_lines
+--
+-- Given a list of strings, produces a list of lists of integers.
+build_board_from_lines :: [String] -> [[Int]]
+build_board_from_lines lines = map read_numbers_from_line lines
+
+-- is_valid_sudoku_line
+--
+-- Ensures that a given line has the appropriate number of values and that they
+-- all sum to the appropriate value (45).
+is_valid_sudoku_line :: [Int] -> Bool
+is_valid_sudoku_line numbers =
+    length numbers == (length (nub numbers)) &&
+    length numbers == 9 &&
+    all (>= 0) numbers &&
+    sum numbers == 45
+
+-- is_valid_sudoku_board
+--
+-- Verifies that a board is valid.
+is_valid_sudoku_board :: [[Int]] -> Bool
+is_valid_sudoku_board board = all is_valid_sudoku_line board
+
 -- read_file
 --
 -- Given a filename (as a string), attempts to read a sudoku board from the
@@ -36,4 +92,10 @@ read_file filename = do
         -- It does not. Quit.
         print_error ("File does not exist: '" ++ filename ++ "'")
     else do
-        putStrLn "Reading from file."
+        -- It does exist! Let's load it.
+        contents <- readFile filename
+        let board = build_board_from_lines $ filter (not . null) $ splitOn "\n" contents
+        if is_valid_sudoku_board board then do
+            putStrLn "Valid"
+        else do
+            putStrLn "Invalid"
