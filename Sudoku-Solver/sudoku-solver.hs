a1b8012c8156cc3dc0569f35b34faa4fb33b865e
Pierce Darragh <pierce.darragh@gmail.com>
2016-01-25 22:14:43 -0700

    Backtracking solving
    Implementing backtracking. It was a pain.
    
    It seems that, occasionally, `test6.sudoku` results in either an infinite loop somewhere or the error `Can not make a guess and no guesses to backtrack.` Somewhere some value is not being passed correctly. Should probably rectify that.

--- Sudoku-Solver/sudoku-solver.hs
+++ Sudoku-Solver/sudoku-solver.hs
@@ -17,9 +17,17 @@ import System.Directory
 import System.Environment
 import System.Exit
 import System.IO
+import System.Random
 
 type Value = Int
 type Board = [Maybe Value]
+
+data Guess = Guess {
+    guessIndex      :: Int,
+    guessValue      :: Value,
+    guessOptions    :: [Value],
+    guessBoard      :: Board
+} deriving (Show)
 
 {---------------
 
@@ -268,6 +276,38 @@ group_is_correct group = and [
     ]
     where values = map value_from_maybe group
 
+-- value_in_range
+--
+-- Checks if a Maybe Value is within a range.
+value_in_range :: Maybe Value -> Int -> Bool
+value_in_range Nothing _ = False
+value_in_range (Just value) max = elem value [1..max]
+
+-- incomplete_group_is_correct
+--
+-- Tests whether a list of values is not incorrect. Effectively just checks that
+-- all values in a group are:
+--   1. unique
+--   2. not outside the relevant range
+incomplete_group_is_correct :: [Maybe Value] -> Int -> Bool
+incomplete_group_is_correct group max
+    = and [
+        nonblanks == nub nonblanks ,                    -- elements are unique
+        all id $ map (`value_in_range` max) nonblanks   -- elements within group
+    ]
+    where nonblanks = filter (not . isNothing) group
+
+-- incomplete_board_is_correct
+--
+-- If a board has empty spaces, this checks that none of the filled spaces are
+-- necessarily wrong.
+incomplete_board_is_correct :: Board -> Int -> Int -> Bool
+incomplete_board_is_correct board m n = and [
+        all id $ map (`incomplete_group_is_correct` (m * n)) $ rows_from_board board m n ,
+        all id $ map (`incomplete_group_is_correct` (m * n)) $ cols_from_board board m n ,
+        all id $ map (`incomplete_group_is_correct` (m * n)) $ groups_from_board board m n
+    ]
+
 {---------------
 
     SOLVE
@@ -467,6 +507,20 @@ filter_possibilities possibilities values
 -- Solving the Board
 --------
 
+-- attempt_solution_and_print_board
+--
+-- Takes a board (and its dimensions) and attempts to solve the board and print
+-- the result.
+attempt_solution_and_print_board :: Board -> Int -> Int -> IO ()
+attempt_solution_and_print_board board m n = do
+    (solved, solution) <- solve_board_with_backtracking board m n []
+    if solved
+        then print_board solution m n
+        else do
+            putStrLn "Could not compute solution."
+            putStrLn ""
+            print_board solution m n
+
 -- solve_board_step
 --
 -- Searches the entire board for blank squares to which there is only one
@@ -482,19 +536,140 @@ solve_board_step board m n
 -- is found, the completed board is returned with a True.
 solve_board :: Board -> Int -> Int -> (Bool, Board)
 solve_board board m n
-    | board_solved       = (True, board)
-    | board == new_board = (False, board)
-    | otherwise          = solve_board new_board m n
+    | board_solved       = (True, board)    -- board is filled out
+    | board == new_board = (False, board)   -- board is not filled but cannot proceed
+    | otherwise          = solve_board new_board m n    -- recur!
     where
-        board_solved = null $ filter (== True) $ map isNothing board
+        board_solved = null $ filter isNothing board
         new_board    = solve_board_step board m n
 
-attempt_solution_and_print_board :: Board -> Int -> Int -> IO ()
-attempt_solution_and_print_board board m n
-    | solved = print_board solution m n
+-- need: a method which takes an index and a list of values and makes a random
+-- selection from among the values and puts it in the specified index
+
+-- guess_from_values
+--
+-- Makes a random selection from a list of values.
+-- The random seed is discarded because we don't want reproducibility. Gasp!
+guess_from_values :: [Value] -> IO Value
+guess_from_values [] = error "No values to choose from."
+guess_from_values values = do
+    random_gen <- newStdGen
+    let (index, _) = randomR (0, (length values) - 1) random_gen
+    return $ values !! index
+
+-- a method which takes a board and makes a random move on it, returning
+-- the necessary values to track that change
+-- returns: (Int, Value, [Value], Board)
+-- meaning: (index, chosen value, non-chosen values, new board)
+make_guess_for_board :: Board -> Int -> Int -> IO Guess
+make_guess_for_board board m n
+    | null empty_indices = error "no blank spots to guess"
     | otherwise = do
-        putStrLn "Could not compute solution."
-        putStrLn ""
-        print_board solution m n
+        random_gen <- newStdGen
+        let
+            (i, g)          = randomR (0, (length empty_indices) - 1) random_gen
+            index           = empty_indices !! i
+            possibilities   = possibilities_for_index board m n index
+        if (null possibilities)
+            then make_guess_for_board board m n
+            else do
+                --putStrLn "make_guess_for_board"
+                value <- guess_from_values possibilities
+                let new_board = replace_pair board (index, Just value)
+                return Guess {
+                    guessIndex      = index,
+                    guessValue      = value,
+                    guessOptions    = (delete value possibilities),
+                    guessBoard      = board
+                }
+    where 
+        empty_indices   = map fst $ filter (isNothing . snd)
+                            $ zip [0..(m*n) * (m*n) - 1] board
+
+-- board_can_be_guessed
+--
+-- Determines whether a board is in a position where a guess can be made.
+board_can_be_guessed :: Board -> Int -> Int -> Bool
+board_can_be_guessed board m n
+    | null $ filter isNothing board
+        -- No spots to guess.
+        = False
+    | otherwise
+        -- Check whether any of the blank spaces have no possibilities.
+        -- If any have none, then something is wrong and the board cannot proceed.
+        = null $ filter null $ map (possibilities_for_index board m n) $ map fst $ filter (isNothing . snd) $ zip [0..] board
+
+-- guess_on_board
+--
+-- Takes a board with blank spaces and makes a guess, returning the guess and
+-- the new board state.
+guess_on_board :: Board -> Int -> Int -> IO (Guess, Board)
+guess_on_board board m n = do
+    guess <- make_guess_for_board board m n
+    return (guess, replace_pair board (guessIndex guess, Just (guessValue guess)))
+
+-- backtrack_board
+--
+-- Given a board and a list of previous guesses, this attempts to rectify issues
+-- caused by erroneous guesses made. It starts with the most-recent guess and
+-- first tries to check if a different value would work better for the same
+-- index. If not, it backtracks an additional guess. If there are no guesses to
+-- backtrack, a new guess is made.
+backtrack_board :: Board -> Int -> Int -> [Guess] -> IO ([Guess], Board)
+backtrack_board board m n []
+    -- No guesses have been made... so make a new one.
+    | board_can_be_guessed board m n = do
+        (guess, new_board) <- guess_on_board board m n
+        return ([guess], new_board)
+    | otherwise = error "Can not make a guess and no guesses to backtrack."
+backtrack_board board m n (guess:old_guesses)
+    -- There are some previous guesses, so try to backtrack.
+    | null $ guessOptions guess
+        -- We've tried everything for this index; backtrack again.
+        = backtrack_board board m n old_guesses
+    | otherwise = do
+        -- Try something else for this index.
+        new_guess_value <- guess_from_values $ guessOptions guess
+        let
+            new_possibilities   = delete new_guess_value $ guessOptions guess
+            new_board           = replace_pair (guessBoard guess) (guessIndex guess, Just new_guess_value)
+            new_guess           = Guess {
+                                    guessIndex      = guessIndex guess,
+                                    guessValue      = new_guess_value,
+                                    guessOptions    = new_possibilities,
+                                    guessBoard      = board
+                                }
+            board_correct       = board_is_correct new_board m n
+        return ((new_guess:old_guesses), new_board)
+
+-- solve_board_with_backtracking
+--
+-- Takes a board and attempts to find a solution using the conventional "fill
+-- all the possibilities" method. If that fails (there are blank spots left),
+-- a guess is made. If the board ever ceases to be solvable, the guesses are
+-- undone until they work.
+solve_board_with_backtracking :: Board -> Int -> Int -> [Guess] -> IO (Bool, Board)
+solve_board_with_backtracking board m n guesses
+    | null $ filter isNothing board
+        -- The board has been filled in.
+        = if (board_is_correct board m n)
+            then return (True, board)   -- it's done!
+            else backtrack              -- something went wrong...
+    | incomplete_board_is_correct board m n
+        -- The board is not filled, but is so far correct.
+        = if board == next_board
+            then do
+                -- Need to take a guess (no progress via iteration).
+                (guess, guess_board) <- guess_on_board board m n
+                solve_board_with_backtracking guess_board m n (guess:guesses)
+            else do
+                -- Use the board with more squares and continue.
+                solve_board_with_backtracking next_board m n guesses
+    | otherwise
+        -- The board is not filled nor is it correct... so let's rewind.
+        = backtrack
     where
-        (solved, solution) = solve_board board m n
+        (next_board_complete, next_board) = solve_board board m n
+        backtrack = do
+            (backtracked_guesses, backtracked_board) <- backtrack_board board m n guesses
+            solve_board_with_backtracking backtracked_board m n backtracked_guesses
